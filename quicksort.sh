aee12ed91e4bbdcce780adeeb6fe83b49016638a
Jerico <1747655736@qq.com>
2024-04-14 19:36:45 +0800

    update .sh

--- quicksort.sh
+++ quicksort.sh
@@ -3,6 +3,7 @@ as -gstabs -o partition.o partition.s
 as -gstabs -o quicksort.o quicksort.s
 gcc -g -static -DTEST -o quicksort partition.o quicksort.o quicksort-main.c
 for i in {1..25}; do
+    echo $i
     ./quicksort
     sleep 0.3s
 done
